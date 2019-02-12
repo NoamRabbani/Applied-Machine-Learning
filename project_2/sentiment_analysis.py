f598524f30f69a702a2fd2473711836f6d800a9b
florentjiang <florentj@DESKTOP-UEC8I82.localdomain>
2019-02-11 21:21:20 -0500

    implement naive bayes classifier

--- project_2/sentiment_analysis.py
+++ project_2/sentiment_analysis.py
@@ -1,9 +1,13 @@
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.model_selection import train_test_split
+import pdb
 import numpy as np
 import pandas as pd
 import os
 import re
+import csv
 
 """
 Step 2: Read into Python
@@ -14,7 +18,7 @@ train_data_dir = os.path.join('.', 'data', 'train')
 train_data_dir = os.path.join('.', 'data', 'train')
 test_data_dir = os.path.join('.', 'data', 'test')
 
-subset = 1000
+subset = 25000
 reviews_train = []
 for sentiment in ['pos', 'neg']:
     path = os.path.join(train_data_dir, sentiment)
@@ -76,4 +80,55 @@ X = cv.transform(reviews_train)
 X_test = cv.transform(reviews_test)
 
 # X : sparse matrix, [n_samples, n_features]
+print(X.shape)
 print(X[5, 5])
+
+target = [1 if i < 12500 else 0 for i in range(25000)]
+
+X_train, X_val, y_train, y_val = train_test_split(
+            X, target, train_size = 0.1, test_size =0.1)
+bnb = BernoulliNB()
+print('size train x: '+str(X_train.shape))
+print('size train y: '+str(len(y_train)))
+
+print('size val x: '+str(X_val.shape))
+print('size val y: '+str(len(y_val)))
+bnb.fit( X_train,y_train)
+
+y_pred = bnb.predict(X_val)
+#y_output=pd.DataFrame
+print('predicted by scikit')
+print(y_pred)
+
+class MyBernoulliNB(object):
+    def __init__(self, alpha=1.0):
+        self.alpha = alpha
+
+    def fit(self, X, y):
+        count_sample = X.shape[0]
+        separated = [[x for x, t in zip(X, y) if t == c] for c in np.unique(y)]
+        self.class_log_prior_ = [np.log(len(i) / count_sample) for i in separated]
+      #  pdb.set_trace()
+        count = np.array([np.array(i).sum(axis=0) for i in separated]) + self.alpha
+        smoothing = 2 * self.alpha
+        n_doc = np.array([len(i) + smoothing for i in separated])
+        print(n_doc)
+        self.feature_prob_ = count / n_doc[np.newaxis].T
+        return self
+    def predict_log_proba(self, X):
+   #     pdb.set_trace()
+        return [(np.log(self.feature_prob_) * x + \
+                 np.log(1 - self.feature_prob_) * np.abs(x - 1)
+                 ).sum(axis=1) + self.class_log_prior_ for x in X]
+    def predict(self, X):
+        return np.argmax(self.predict_log_proba(X), axis=1)
+nb = MyBernoulliNB(alpha=1).fit(X_train.toarray() , y_train)
+
+print('predicted by my implementation')
+my_y_pred=nb.predict(X_val.toarray())
+print(my_y_pred)
+resultfile = open("output.csv",'w')
+
+for indx in range(len(y_pred)):
+    resultfile.write(str(y_pred[indx]) + ", "+str(my_y_pred[indx])+"\n")
+resultfile.close()
