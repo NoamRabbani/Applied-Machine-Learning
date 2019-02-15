6a6992cc46a13caf66ecc09f51b79f323f5ae197
svysali <shivashree.vaidhyamsubramanian@mail.mcgill.ca>
2019-02-14 22:06:48 -0500

    Completed validation

--- project_2/classifiers.py
+++ project_2/classifiers.py
@@ -9,6 +9,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.svm import LinearSVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import cross_val_score
 
 def getSVM(X,target,c_initial=0.01,select_c=False):
     X_train, X_val, y_train, y_val = train_test_split(
@@ -26,10 +27,13 @@ def getSVM(X,target,c_initial=0.01,select_c=False):
             print("Accuracy for C={}: {}".format(c, accuracy))
         print("Selected c={}".format(selected_c)) 
     final_svm = LinearSVC(C=selected_c)
-    final_svm.fit(X, target)
-    print("Training Accuracy\t:\t{}".format(
-                accuracy_score(target, final_svm.predict(X))
-            ))
+    final_svm.fit(X_train, y_train)
+    cv_scores = cross_val_score(final_svm, X, target, cv=5)
+    print("Training Accuracy\t:\t{}".format(accuracy_score(y_train, final_svm.predict(X_train))))
+    print("Validation Accuracy\t:\t{}".format(accuracy_score(y_val, final_svm.predict(X_val))))
+    print("Cross validation scores\t:\t{}".format(cv_scores) )
+    print("Average {} {}".format(cv_scores.mean(), cv_scores.std() * 2))
+    
     return final_svm
 
 
@@ -49,8 +53,10 @@ def getLogisticRegression(X,target,c_initial=0.01,select_c=False):
             print("Validation Accuracy for C={}: {}".format(c, accuracy))
         print("Selected c={}".format(selected_c))
     final_log_reg = LogisticRegression(C=selected_c)
-    final_log_reg.fit(X, target)
-    print("Training Accuracy\t:\t{}".format(
-                accuracy_score(target, final_log_reg.predict(X))
-            ))
+    final_log_reg.fit(X_train, y_train)
+    cv_scores = cross_val_score(final_log_reg, X, target, cv=5)
+    print("Training Accuracy\t:\t{}".format(accuracy_score(y_train, final_log_reg.predict(X_train))))
+    print("Validation Accuracy\t:\t{}".format(accuracy_score(y_val, final_log_reg.predict(X_val))))
+    print("Cross validation scores\t:\t{}".format(cv_scores))
+    print("Average {} {}".format(cv_scores.mean(), cv_scores.std() * 2))
     return final_log_reg
--- project_2/sentiment_analysis.py
+++ project_2/sentiment_analysis.py
@@ -1,13 +1,8 @@
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
-from sklearn.metrics import accuracy_score
-from sklearn.naive_bayes import BernoulliNB
-from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline
-from feature_extraction import remove_stop_words,get_lemmatized_text,get_stemmed_text
+from feature_extraction import remove_stop_words,get_lemmatized_text
+from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
 from classifiers import getSVM, getLogisticRegression
-import pdb
+from sklearn.model_selection import train_test_split
 import numpy as np
-import sys
 import os
 import re
 import csv
@@ -75,8 +70,11 @@ def write_output_csv(output_file,y_pred):
 reviews_train_clean = preprocess_reviews(reviews_train)
 reviews_test_clean = preprocess_reviews(reviews_test)
 
+no_sw_train = remove_stop_words(reviews_train_clean) 
+no_sw_test = remove_stop_words(reviews_test_clean)
 
-
+lemmatized_train = get_lemmatized_text(reviews_train_clean) 
+lemmatized_test = get_lemmatized_text(reviews_test_clean)
 """
 Data set with no processing
 """
@@ -85,34 +83,13 @@ target = [1 if i < 12500 else 0 for i in range(25000)]
 
 # https://scikit-learn.org/stable/modules/generated/sklearn.feature_extraction.text.CountVectorizer.html
 #only count
-#cv = CountVectorizer(binary=True)
-#cv.fit(reviews_train)
-#X = cv.transform(reviews_train)
-#X_test = cv.transform(reviews_test)
+cv = CountVectorizer(binary=True)
+cv.fit(reviews_train)
+X = cv.transform(reviews_train)
+X_test = cv.transform(reviews_test)
 
-## X : sparse matrix, [n_samples, n_features]
-##print(X.shape)
-##print(X[5, 5])
-#
-#X_train, X_val, y_train, y_val = train_test_split(
-#            X, target, train_size = 0.75, test_size =0.25)
-
-"""
-naive bayes from scikit - only for comparing our implementation
-"""
-
-#bnb = BernoulliNB()
-#print('size train x: '+str(X_train.shape))
-#print('size train y: '+str(len(y_train)))
-#
-#print('size val x: '+str(X_val.shape))
-#print('size val y: '+str(len(y_val)))
-#bnb.fit( X_train,y_train)
-#
-#y_pred = bnb.predict(X_val)
-#
-#print('predicted by scikit')
-#print(y_pred)
+X_train, X_val, y_train, y_val = train_test_split(
+            X, target, train_size = 0.75, test_size =0.25)
 
 """
 naive bayes implementation
@@ -163,59 +140,38 @@ Task 1 : Running Naive Bayes from scratch
 Task 2 : Running Two Different classifiers
 """
 
-print("############################################")
-print("# 2.1 Support Vector Machine with bigrams  #")
-print("############################################")
+print("##########################################################")
+print("# 2.1 Classifier : Support Vector Machine with bigrams   #")
+print("##########################################################")
 #unigram and bigram count
 ngram_vectorizer = CountVectorizer(binary=True, ngram_range=(1, 2))
 ngram_vectorizer.fit(reviews_train_clean)
 X_bigram = ngram_vectorizer.transform(reviews_train_clean)
 model = getSVM(X_bigram,target)
-
-print("############################################")
-print("# 2.2 Logistic Regression with bigrams     #")
-print("############################################")
+#
+print("##########################################################")
+print("# 2.2 Classifier : Logistic Regression with bigrams      #")
+print("##########################################################")
 model = getLogisticRegression(X_bigram,target)
 
 """
 Task 3 : Feature Extraction Pipelines
 """
-#print("############################################")
-#print("# 3.1 SVM with No all stop words removed   #")
-#print("############################################")
-#no_stop_words_train = remove_stop_words(reviews_train_clean)
-#no_stop_words_test = remove_stop_words(reviews_test_clean)
-#
-#ngram_vectorizer = CountVectorizer(binary=True, ngram_range=(1, 2))
-#ngram_vectorizer.fit(reviews_train_clean)
-#X_no_stop_words = ngram_vectorizer.transform(no_stop_words_train)
-#X_test_no_stop_words = ngram_vectorizer.transform(no_stop_words_test)
-#X_train, X_val, y_train, y_val = train_test_split(
-#    X_no_stop_words, target, train_size = 0.75
-#)
-#max_accuracy_c_svm = 0
-#selected_c = 0.01
-#check_for_c = False
-#if(check_for_c):
-#    for c in [0.01, 0.05, 0.25, 0.5, 1]:
-#        svm = LinearSVC(C=c)
-#        svm.fit(X_train, y_train)
-#        accuracy = accuracy_score(y_val, svm.predict(X_val))
-#        if(accuracy >= max_accuracy_c_svm ):
-#            max_accuracy_c_svm = accuracy
-#            selected_c = c        
-#        print("Accuracy for C={}: {}".format(c, accuracy))
-#print("Selected c={}".format(selected_c)) 
+print("############################################")
+print("# 3.1 Binary Occurences                    #")
+print("############################################")
+bin_vectorizer = CountVectorizer(binary=True)
+bin_vectorizer.fit(reviews_train_clean)
+X_bin = bin_vectorizer.transform(reviews_train_clean)
+model = getSVM(X_bin,target)
 
-#lemmatized_reviews = get_lemmatized_text(reviews_train_clean)
-
-"""
-Task 4 : Validation pipeline
-"""
-
-
-
-
+print("############################################")
+print("# 3.2 TF-IDF                               #")
+print("############################################")
+tfidf_vectorizer = TfidfVectorizer()
+tfidf_vectorizer.fit(reviews_train_clean)
+X_tfidf = tfidf_vectorizer.transform(reviews_train_clean)
+model = getSVM(X_tfidf,target,select_c=True)
 
 """
 Task 5 : Final model
@@ -223,18 +179,21 @@ Task 5 : Final model
 print("############################################")
 print("# Submitted model performances             #")
 print("############################################")
-# 14/02 1/2 - Trigrams + some stop words removed. 
+# 14/02 1/2 - Trigrams + binary
 stop_words = ['in', 'of', 'at', 'a', 'the']
 ngram_vectorizer = CountVectorizer(binary=True, ngram_range=(1, 3), stop_words=stop_words)
 ngram_vectorizer.fit(reviews_train_clean)
-X = ngram_vectorizer.transform(reviews_train_clean)
+X = ngram_vectorizer.transform(reviews_train_clean) 
 X_test = ngram_vectorizer.transform(reviews_test_clean)
 final_model_1 = getSVM(X,target,0.01)
 
 
-"""
-Run predict function of whichever model you want to run and 
-write results to csv to submit to kaggle
-"""
-#Y_pred = final_model_1.predict(X_test_final)
-#write_output_csv("results_final.csv",Y_pred)
+# submit another day
+stop_words = ['in', 'of', 'at', 'a', 'the']
+ngram_vectorizer = CountVectorizer(binary=True, ngram_range=(1, 3), stop_words=stop_words)
+ngram_vectorizer.fit(lemmatized_train)
+X = ngram_vectorizer.transform(lemmatized_train) 
+X_test = ngram_vectorizer.transform(lemmatized_test)
+final_model_1 = getSVM(X,target,0.01)
+#Y_pred = final_model_1.predict(X_test)
+#write_output_csv("results_lemmatized.csv",Y_pred)
